00fb6bf32235a69a480dccb103b4f4884985ed9d
Andreas Finck <andreas.finck@icloud.com>
2025-06-11 10:11:38 +0000

    cleaned measurments.rs

--- src/measurements.rs
+++ src/measurements.rs
@@ -8,14 +8,6 @@ pub struct Measurement {
     pub timestamp: String,
 }
 
-pub fn create_measurement(conn: &Connection, value: f64, timestamp: &str) -> Result<()> {
-    conn.execute(
-        "INSERT INTO measurements (value, timestamp) VALUES (?1, ?2)",
-        params![value, timestamp],
-    )?;
-    Ok(())
-}
-
 pub fn get_measurements(conn: &Connection) -> Result<Vec<Measurement>> {
     let mut stmt = conn.prepare("SELECT id, value, timestamp FROM measurements")?;
     let measurement_iter = stmt.query_map([], |row| {
